ee585d6b3a7705586094a7f5d2c9d3154e1181f7
kevin velazquez <kevine1524@gmail.com>
2022-07-12 00:06:24 -0300

    modificaciones pequeñas

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 //Cotizador de precios y reserva de habitacion en hotel
 
 //Calculador de dias
-// let ida = prompt("¿Que dia llegas(M/D/AÑO)?");
-// let vuelta = prompt ("¿Que dia te vas(M/D/AÑO)?");
+let fechaida = Date (prompt("¿Que dia llegas(M/D/AÑO)?"));
+let fechavuelta = Date(prompt ("¿Que dia te vas(M/D/AÑO)?"));
 
-let fechaida = new Date ('12 12 2010');
-let fechavuelta = new Date ('12 12 2012');
+// let fechaida = new Date (fechadeida);
+// let fechavuelta = new Date (fechadevuelta);
 
 console.log (fechaida);
 console.log (fechavuelta);
@@ -19,20 +19,35 @@ let diferencia = Math.abs( ida - vuelta);
 
 console.log (diferencia);
 
-let registroReserva = new reserva (nombre, apellido, fechaDeLlegada, fechadevuelta, personas, tipodehabitacion) {
-    this.nombre = nombre
-    this.apellido=apellido
-    this.fechaDeLlegada=fechaDeLlegada
-    this.fechadevuelta=fechadevuelta
-    this.personas = personas
-    this.tipodehabitacion=tipodehabitacion
-};
+// class registroReserva = 
+// constructor (nombre, apellido, fechaDeLlegada, fechadevuelta, personas, tipodehabitacion) {
+//     this.nombre = nombre
+//     this.apellido = apellido
+//     this.personas = personas
+//     this.tipodehabitacion = tipodehabitacion
+//     this.fechaDeLlegada = fechaDeLlegada
+//     this.fechadevuelta = fechadevuelta
+// };
+
+const crearReserva = () => {
+    let nombre = prompt("¿Cual es su nombre?");
+    let apellido = prompt("¿Cual es su apellido?");
+    let personas= prompt("¿Cuantas personas se hospedan?");
+    let tipodeHabitacion= prompt("¿Que habitacion desea?");
+    let fechaDeLlegada = prompt("¿Que fecha llega?");
+    let fechadevuelta= prompt("¿Hasta que fecha se hospeda?");
+
+    const reserva = new crearReserva (nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta );
+    return reserva;
+}
 
 // Agregar un ARRAY con objetos de las reservas 
 
-let reservas = [
-    {nombre: "Kevin", apellido: "Velazquez", fechaDeLlegada: '08 09 2022', fechadevuelta: '08 10 2022', personas: 1, tipodehabitacion: "personal" }
-]
+let listaDeReservas = [
+    {nombre: "Kevin", apellido: "Velazquez", fechaDeLlegada: '08 09 2022', fechadevuelta: '08 10 2022', personas: 1, tipodehabitacion: "personal" },
+
+];
+reservas.push (reserva);
 
 
 
