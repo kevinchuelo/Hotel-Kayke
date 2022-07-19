8d25c7b1904c4f0e48eef7e747a7b1a636879be5
kevin velazquez <kevine1524@gmail.com>
2022-07-18 22:47:07 -0300

    desafio arrays ok

--- js/index.js
+++ js/index.js
@@ -1,61 +1,61 @@
 //Cotizador de precios y reserva de habitacion en hotel
 
 let pregida = parseFloat(prompt("¿Que dia llegas(AÑO/M/D)?"));
-let pregvuelta = parseFloat(prompt ("¿Que dia te vas(AÑO/M/D)?"));
-console.log (pregida);
-console.log (pregvuelta);
+let pregvuelta = parseFloat(prompt("¿Que dia te vas(AÑO/M/D)?"));
+console.log(pregida);
+console.log(pregvuelta);
 //Calculador de dias
-let fechaida = Date (pregida);
-let fechavuelta = Date (pregvuelta);
+let fechaida = Date(pregida);
+let fechavuelta = Date(pregvuelta);
 
-console.log (fechaida);
-console.log (fechavuelta);
+console.log(fechaida);
+console.log(fechavuelta);
 
 const milisegundos = 24 * 60 * 60 * 1000;
-console.log (milisegundos);
+console.log(milisegundos);
 
-let ida = Math.round (fechaida / milisegundos);
-let vuelta = Math.round (fechavuelta / milisegundos);
-let diferencia = Math.abs( ida - vuelta);
+let ida = Math.round(fechaida / milisegundos);
+let vuelta = Math.round(fechavuelta / milisegundos);
+let diferencia = Math.abs(ida - vuelta);
 
-alert (diferencia);
+alert(diferencia);
 
-// class registroReserva = 
-// constructor (nombre, apellido, fechaDeLlegada, fechadevuelta, personas, tipodehabitacion) {
-//     this.nombre = nombre
-//     this.apellido = apellido
-//     this.personas = personas
-//     this.tipodehabitacion = tipodehabitacion
-//     this.fechaDeLlegada = fechaDeLlegada
-//     this.fechadevuelta = fechadevuelta
-// };
 
 //array donde se van a guardar los obj con las reservas.
 let listaDeReservas = [
-    {nombre: "Kevin", apellido: "Velazquez", fechaDeLlegada: '08 09 2022', fechadevuelta: '08 10 2022', personas: 1, tipodehabitacion: "personal" },
-
+  { nombre: "Kevin", apellido: "Velazquez", fechaDeLlegada: '08 09 2022', fechadevuelta: '08 10 2022', personas: 1, tipodehabitacion: "personal" },
 ];
+console.log(listaDeReservas);
+
 
 // clase constructora para hacer reservas.
-function constructorreserva (nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta) {
-    this.nombre = prompt("¿Cual es su nombre?");
-    this.apellido = prompt("¿Cual es su apellido?");
-    this.personas = prompt("¿Cuantas personas se hospedan?");
-    this.tipodeHabitacion = prompt("¿Que habitacion desea?");
-    this.fechaDeLlegada = prompt("¿Que fecha llega?");
-    this.fechadevuelta = prompt("¿Hasta que fecha se hospeda?");
-// nose si va esta constante 
-    // const reserva1 = new crearReserva(nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta);
-    // return reserva1;}
-    return constructorreserva;
-}
-console.log (constructorreserva)
-//Funcion que agrega reservas
+class Reserva {
+  constructor(nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.personas = personas;
+    this.tipodeHabitacion = tipodeHabitacion;
+    this.fechaDeLlegada = fechaDeLlegada;
+    this.fechadevuelta = fechadevuelta;
 
-function agregarReserva (reserva) {
-  listaDeReservas.push (constructorreserva);  
-  console.log (listaDeReservas);
+
+  }
 }
+
+const crearReserva = () => {
+  let nombre = prompt("¿Cual es su nombre?");
+  let apellido = prompt("¿Cual es su apellido?");
+  let personas = prompt("¿Cuantas personas se hospedan?");
+  let tipodeHabitacion = prompt("¿Que habitacion desea?");
+  let fechaDeLlegada = prompt("¿Que fecha llega?");
+  let fechadevuelta = prompt("¿Hasta que fecha se hospeda?");
+
+  let nuevaReserva = new Reserva(nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta)
+  listaDeReservas.push(nuevaReserva);
+  return nuevaReserva;
+};
+console.log(listaDeReservas);
+
 
 
 
