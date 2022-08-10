2c60d71172ec44b4bb9d72be388dd1a2a0733ba6
kevin velazquez <kevine1524@gmail.com>
2022-08-10 00:58:16 -0300

    agregue librerias

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
+//Libreria luxon
+let dataTime = luxon.DateTime;
+
+
 //Cotizador de precios y reserva de habitacion en hotel
 //Evento que me llama esta funcion con un click
 //Variable que llama al form donde aplico la funcion
 let btncrearreserva = document.querySelector ("#btnForm");
 //evento que crear la reserva para calcular los dias
 btncrearreserva.addEventListener ( 'click', () => {
-  let pregida = document.querySelector ("#check-in").value
+let pregida = document.querySelector ("#check-in").value
 let pregvuelta = document.querySelector ("#check-out").value
 const cantidaddepersonas = document.querySelector ("#personas").value;
 //Calculador de dias
@@ -12,20 +16,18 @@ let fechaida = Date.parse(pregida);
 let fechavuelta = Date.parse(pregvuelta);
 const milisegundos = 86400000;
 let diferenciadias = Math.abs((fechaida - fechavuelta) / milisegundos);
-alert(`Los dias que te vas a quedar en el hotel son ${diferenciadias} y la cantidad de personas que se hospedan son ${cantidaddepersonas}`);
-});
+let completarform = document.querySelector ("#formulario")
+// const p = document.createElement("");
+// p.setAttribute("id","extensionForm");
+// p.innerText(`Los dias que te vas a quedar en el hotel son ${diferenciadias} y la cantidad de personas que se hospedan son ${cantidaddepersonas}`);
+// document.body.completarform.appendChild(p);
 
-
-// function cotizarReserva (event){
-// let pregida = parseFloat(prompt("¿Que dia llegas(AÑO/M/D)?"))
-// let pregvuelta = parseFloat(prompt("¿Que dia te vas(AÑO/M/D)?"))
-// console.log (pregida);
-// console.log (pregvuelta);
-// let reservacreada = new nuevaReserva = pregida + pregvuelta;
-// return reservacreada;
-//  }
-
-
+Toastify (
+  {text:`Los dias que te vas a quedar en el hotel son ${diferenciadias} y la cantidad de personas que se hospedan son ${cantidaddepersonas}`,
+  duration:5000 ,
+  position: "center",
+  style:{background: "#84b6f4",},
+}).showToast();});
 //FUNCIONES PARA GUARDAR DATOS DE RESERVAS EN EL HOTEL
 //array donde se van a guardar los obj con las reservas.
 let listaDeReservas = [
@@ -33,8 +35,6 @@ let listaDeReservas = [
   { nombre: "Carlos", apellido: "Saez", fechaDeLlegada: '25 09 2022', fechaDeVuelta: '08 11 2022', personas: 4, tipodehabitacion: "familiar" },
   { nombre: "Ramon", apellido: "Perez", fechaDeLlegada: '10 10 2022', fechaDeVuelta: '20 10 2022', personas: 1, tipodehabitacion: "personal" }
 ];
-
-
 // clase constructora para hacer reservas.
 class Reserva {
   constructor(nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechadevuelta) {
@@ -48,12 +48,12 @@ class Reserva {
 }
 
 const crearReserva = () => {
-  let nombre = prompt("¿Cual es su nombre?");
-  let apellido = prompt("¿Cual es su apellido?");
-  let personas = prompt("¿Cuantas personas se hospedan?").parseInt;
-  let tipodeHabitacion = prompt("¿Que habitacion desea?");
-  let fechaDeLlegada = prompt("¿Que fecha llega?").parseFloat;
-  let fechaDeVuelta = prompt("¿Hasta que fecha se hospeda?").parseFloat;
+  let nombre = cajanombre;
+  let apellido = cajaapellido;
+  let personas = cantidaddepersonas;
+  let tipodeHabitacion = cajahabitacion;
+  let fechaDeLlegada = pregida;
+  let fechaDeVuelta = pregvuelta;
 
   let nuevaReserva = new Reserva(nombre, apellido, personas, tipodeHabitacion, fechaDeLlegada, fechaDeVuelta)
   listaDeReservas.push(nuevaReserva);
@@ -61,9 +61,12 @@ const crearReserva = () => {
 };
 
 //Boton para ver la lista de reservas hechas hecho com metodo de busqueda foreach
-listaDeReservas.forEach (function verReservas (nombre, fechaDeLlegada, fechaDeVuelta) {
-console.log (`La reserva fue hecha por ${nombre}, le fecha de llegada es ${fechaDeLlegada} y su fecha de vuelta es ${fechaDeVuelta}.`)
-}); 
+// listaDeReservas.forEach (function verReservas (nombre, fechaDeLlegada, fechaDeVuelta) {
+// Toastify({ text:`La reserva fue hecha por ${nombre}, le fecha de llegada es ${fechaDeLlegada} y su fecha de vuelta es ${fechaDeVuelta}.`,
+// duration: 5000,
+// position: "center",
+// }).showToast(); });
+
 //Crear funcion para aplicarcela al metodo y luego poder asignarle esa funcion a un boton en la pagina
 
 //Crear funcion y un metodo de filtrado para poder ver los dias disponibles que hay para rerservar.
@@ -76,9 +79,6 @@ console.log (`La reserva fue hecha por ${nombre}, le fecha de llegada es ${fecha
 //   (el)=> el.nombre == buscador
 // );
 // console.log (search)
-  
-
-//eventos...
 
 
  
